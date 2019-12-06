91dd2409c16596efa3a3b99f85636b99300ff040
KaliningradG <58448600+KaliningradG@users.noreply.github.com>
2019-12-06 17:53:01 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,23 @@ import discord
 from discord.ext import commands
 
 client = discord.Client()
-client = commands.Bot(command_prefix = '!')
-client.remove_command('help')
 
-@client.command()
-async def test(ctx):
-    await ctx.send("This is a test!")
+@client.event  # event decorator/wrapper
+async def on_ready():
+	print(f"We have logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+	print(f"{message.channel}: {message.author} {message.author.name} {message.content}")
+
+	if "ha" in message.content.lower():
+		await message.channel.send("Answer `!yes` or `!no`")
+
+@client.event
+async def on_ready():
+	await client.change_presence(status=discord.Status.idle, activity=discord.Game("Touhou 12.3: Hisoutensoku"))
+	print("okeyyy")
+
+
 
 client.run("YOU-TOKEN-BOT")
