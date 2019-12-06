8c49bf1aa853b5900287d247e00cf250bc171e2b
KaliningradG <58448600+KaliningradG@users.noreply.github.com>
2019-12-06 17:43:10 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,4 +9,4 @@ client.remove_command('help')
 async def test(ctx):
     await ctx.send("This is a test!")
 
-client.run("NjUyMjU0MDM5MTg2MDc5ODA1.XepdjA.QhpDsuYMVCupGZHjETdQl3MZF34")
+client.run("YOU-TOKEN-BOT")
